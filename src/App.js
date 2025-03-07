682225fcbd3e4889d2e6bb4df2e95b77788edf3f
fatmaAlzu <fatma.alzubaidi1@ucalgary.ca>
2025-03-07 12:27:29 -0700

    ex 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const currentYear = new Date().getFullYear(); 
+  const isLoggedIn = true;
+  return(
+    <div>
+      <h1> ENSF-381: Full Stack Web Development</h1>
+      <p>React Components</p>
+      <p> Current Year: {currentYear}</p>
+      <p> {isLoggedIn ? "welcome back!" : "Please log in."} </p>
+      
     </div>
-  );
+  ) 
 }
 
 export default App;
